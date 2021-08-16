74133fa938a7da60a154adb091d9c91150f5233f
david matovu <davidmatovu88@gmail.com>
2021-08-16 18:31:48 +0300

    worked on the landing page in the school system

---
+++ school-project/mystyles/kis.js
@@ -0,0 +1,6 @@
+const hello = "Hey there";
+console.log(hello);
+document.getElementById('demo').innerHTML=hello;
+
+let age = prompt('enter your age');
+document.getElementById('demo').innerHTML= age;
